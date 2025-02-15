2ebc22d4205876ca758a203c21d82f43eba95268
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-02-15 04:43:10 -0500

    Fix enchanting table hardness

--- src/main/java/dev/creoii/greatbigworld/swordsandshields/SwordsAndShields.java
+++ src/main/java/dev/creoii/greatbigworld/swordsandshields/SwordsAndShields.java
@@ -1,10 +1,12 @@
 package dev.creoii.greatbigworld.swordsandshields;
 
+import dev.creoii.greatbigworld.mixin.AbstractBlockStateAccessor;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.Blocks;
 
 public class SwordsAndShields implements ModInitializer {
     @Override
     public void onInitialize() {
-        //BlockModification.INSTANCE.setHardness(Blocks.ENCHANTING_TABLE, 15f);
+        ((AbstractBlockStateAccessor) Blocks.ENCHANTING_TABLE.getDefaultState()).setHardness(15f);
     }
 }
