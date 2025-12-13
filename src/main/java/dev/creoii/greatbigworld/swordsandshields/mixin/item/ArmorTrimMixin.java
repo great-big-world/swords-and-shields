4b97cdbf693ebfc124806f0c7481b2d743165b1e
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-12-13 03:27:00 -0500

    Reformat upgrade tooltip

---
+++ src/main/java/dev/creoii/greatbigworld/swordsandshields/mixin/item/ArmorTrimMixin.java
@@ -0,0 +1,26 @@
+package dev.creoii.greatbigworld.swordsandshields.mixin.item;
+
+import net.minecraft.core.Holder;
+import net.minecraft.core.component.DataComponentGetter;
+import net.minecraft.network.chat.CommonComponents;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.equipment.trim.ArmorTrim;
+import net.minecraft.world.item.equipment.trim.TrimMaterial;
+import net.minecraft.world.item.equipment.trim.TrimPattern;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.function.Consumer;
+
+@Mixin(ArmorTrim.class)
+public record ArmorTrimMixin(Holder<TrimMaterial> material, Holder<TrimPattern> pattern) {
+    @Inject(method = "addToTooltip", at = @At(value = "INVOKE", target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V", ordinal = 1), cancellable = true)
+    private void gbw$reformatArmorTrimTooltip(Item.TooltipContext tooltipContext, Consumer<Component> consumer, TooltipFlag tooltipFlag, DataComponentGetter dataComponentGetter, CallbackInfo ci) {
+        consumer.accept(CommonComponents.space().append(material.value().description()).append(CommonComponents.space().append(pattern.value().copyWithStyle(material))));
+        ci.cancel();
+    }
+}
